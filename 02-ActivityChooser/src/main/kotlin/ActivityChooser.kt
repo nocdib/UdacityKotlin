222408cc299b23dfaf44491e461e5468faf0e16d
Gregory Joshua <greg.joshua@pricer.com>
2021-07-15 16:34:01 +0200

    Do quiz 3-19

--- 02-ActivityChooser/src/main/kotlin/ActivityChooser.kt
+++ 02-ActivityChooser/src/main/kotlin/ActivityChooser.kt
@@ -1,28 +1,76 @@
+val moods: Array<String> = arrayOf("happy", "sad", "angry")
+val conditions: Array<String> = arrayOf("sunny", "cloudy", "rainy", "snowy")
 
 fun main(args: Array<String>) {
-    println(whatShouldIDoToday("happy"))
-    println(whatShouldIDoToday("sad", "rainy"))
-    println(whatShouldIDoToday("happy", "snowy"))
-}
+    // Get the user's mood
+    print("\nWhat is your mood today? ")
+    var mood: String = readLine().toString()?.trim()
+    mood = if (moods.contains(mood.lowercase())) mood.lowercase() else moods.first()
+    // Get today's weather
+    print("\nWhat is today's weather? ")
+    var weather: String = readLine().toString()?.trim()
+    weather = if (conditions.contains(weather.lowercase())) weather.lowercase() else conditions.first()
+    // Get today's temperature
+    print("\nWhat is today's weather? ")
+    var temperature: Int = readLine()?.toIntOrNull() ?: 20
+    // Decide what to do
+    println(whatShouldIDoToday(mood, weather, temperature))
 
-fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24): String {
-    when(mood.lowercase()) {
+}
+fun isHot(temperature: Int) = temperature > 20
+
+fun isCold(temperature: Int) = temperature < 10
+
+
+fun whatShouldIDoToday(mood: String, weather: String, temperature: Int ): String {
+    when(mood) {
         "happy" ->
-            return when(weather.lowercase()) {
-                "sunny" -> "Go to the beach"
-                "snowy" -> "Go play in the snow"
-                "rainy" -> "Read a book"
-                else -> "Stay home"
+            return when (weather) {
+                "sunny" ->
+                    when {
+                        isHot(temperature) -> "Go to the beach"
+                        isCold(temperature) -> "Go to the gym"
+                        else -> "Go to the park"
+                    }
+                "snowy" -> "Play in the snow"
+                "rainy" ->
+                    when {
+                        isCold(temperature) -> "Go to the gym"
+                        else -> "Read a book"
+                    }
+                else -> "Do nothing"
             }
         "sad" ->
-            return when(weather.lowercase()) {
-                "sunny" -> "Go to the beach"
-                "snowy" -> "Watch TV"
-                "rainy" -> "Go to the gym"
-                else -> "Stay home"
+            return when (weather) {
+                "sunny" ->
+                    when {
+                        isHot(temperature) -> "Go to the beach"
+                        else -> "Go to the park"
+                    }
+                "snowy" -> "Go to the gym"
+                "rainy" ->
+                    when {
+                        isHot(temperature) -> "Read a book"
+                        else -> "Go to the gym"
+                    }
+                else -> "Do nothing"
             }
-        else -> "Stay home"
-
+        "angry" ->
+            return when (weather) {
+                "sunny" ->
+                    when {
+                        isHot(temperature) -> "Go to the beach"
+                        else -> "Go to the park"
+                    }
+                "snowy" -> "Go to the gym"
+                "rainy" ->
+                    when {
+                        isHot(temperature) -> "Go to the beach"
+                        else -> "Go to the park"
+                    }
+                else -> "Do nothing"
+            }
+        else -> "Do nothing"
     }
-    return ""
+    return "Do nothing"
 }
